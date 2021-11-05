b9fb399c9f9f5b59479e162a18c4ecb2936307d9
Thomas FERREIRA <thomas.ferreira@kafein-studio.fr>
2021-11-05 12:36:11 +0100

    code propre

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,39 +1,39 @@
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-    Link
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link,
 } from "react-router-dom";
 
-import React from 'react';
+import React from "react";
 
-import Home from '../screens/home';
-import Search from '../screens/search';
-import Recette from '../screens/recette';
-import Login from '../screens/login';
+import Home from "../screens/home";
+import Search from "../screens/search";
+import Recette from "../screens/recette";
+import Login from "../screens/login";
 
 const Routes = () => {
-    return(
+  return (
     <div>
-        <Router>
-            <Switch>
-                <Route exact path="/">
-                    <Home></Home>
-                </Route>
-                <Route path="/search">
-                    <Search></Search>
-                </Route>
-                <Route path="/recette">
-                    <Recette></Recette>
-                </Route>
-                <Route path="/login">
-                    <Login></Login>
-                </Route>
-            </Switch>
-        </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/search">
+            <Search></Search>
+          </Route>
+          <Route path="/recette">
+            <Recette></Recette>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
     </div>
-    )
-}
+  );
+};
 
-export default Routes
+export default Routes;
