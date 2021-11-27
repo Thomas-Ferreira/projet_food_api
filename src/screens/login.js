4d9d9e5e149ebe64d530d915db5f716670d9c384
PierreLeBris <pierre.lebris@outlook.com>
2021-11-27 15:56:10 +0100

    Update login.js

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,10 +1,12 @@
 import '../App.css'
 import React from 'react'
 import LoginForm from '../components/loginForm'
+import i18n from 'i18next'
 
 const search = () => {
   return (
     <div>
+      <p>{i18n.t('menu')}</p>
       <LoginForm></LoginForm>
     </div>
   )
